ab535acb40b5bb44931471f3a32184f5f961cef1
Roger Neal <rneal@unr.edu>
2019-09-05 21:31:31 -0700

    division by 3

---
+++ division.py
@@ -0,0 +1,15 @@
+#% - Modulo
+
+# print remainder for dividing each integer from 0 to 10 by 3
+# 0 % 3 = 0
+# 1 % 3 = 1
+# 2 % 3 = 2
+# 3 % 3 = 0
+# 4 % 3 = 1
+
+#for
+#range
+# %
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for number in range(0, 11):
+    print("= " + str(number%3))
