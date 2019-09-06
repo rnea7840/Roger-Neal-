2e17bededaa9ce48daf799923a555aab7c8a10bf
Roger Neal <rneal@unr.edu>
2019-09-05 21:04:40 -0700

    fourth change

---
+++ fileinput2.py
@@ -0,0 +1,15 @@
+import sys
+# argv = [filename 5, 3]
+
+
+if len(sys.argv) !=3:
+print("usage: python " + sys.argv[0] + "<first integer> <second integer>")
+sys.exit()
+
+firstArg = int(sys.argv[1])
+secondArg = int(sys.argv[2])
+
+
+print(firstArg * secondArg )
+
+
